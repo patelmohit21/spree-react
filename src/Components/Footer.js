96164b2ef234a386c461124c60d229e70f00827a
mohit <patelmohit672@gmail.com>
2024-07-16 15:40:41 +0530

    home page commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,35 +1,42 @@
 import React from "react";
 import "./Footer.css";
+import spreelogo from '../spree_logo.png';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <hr className="full-width-hr" />
-      <div className="contact-section">
-      <h4>Contact US</h4>
-        <p>Spree Demo Site</p>
-        <p>Email: support@spree.com</p>
+      <div className="footer-content">
+        <div className="footer-logo">
+          <img src={spreelogo} alt="" />
+        </div>
+        <div className="contact-section">
+          <h4>Contact us</h4>
+          <p>Spree Demo Site</p>
+          <p>Email: support@example.com</p>
+        </div>
+        <div className="social-section">
+          <h4 style={{marginLeft: "10px"}}>Follow us</h4>
+          <ul className="social-icons">
+            <li>
+              <a href="#">
+                <i className="fab fa-facebook"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <i className="fab fa-instagram"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="footer-section">
-        <h4>Follow us</h4>
-        <ul className="social-icons">
-          <li>
-            <a href="#">
-              <i className="fab fa-facebook"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i className="fab fa-instagram"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
+
       <hr className="partial-width-hr" />
       <div className="footer-container">
         <div className="footer-section">
